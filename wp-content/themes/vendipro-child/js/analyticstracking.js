5a83e5fff8d32f38ea53c5535d8bb339b9edc973
anito <axelnitzschner@mac.com>
2018-05-25 16:38:51 +0200

    js

--- wp-content/themes/vendipro-child/js/analyticstracking.js
+++ wp-content/themes/vendipro-child/js/analyticstracking.js
@@ -5,4 +5,3 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-88792343-1', 'auto', 'clientTracker');
 ga('clientTracker.send', 'pageview');
-console.log(user)
