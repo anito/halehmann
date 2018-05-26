bd794efcabdebd82096a6d292857601248934a7b
anito <axelnitzschner@mac.com>
2018-05-26 04:49:38 +0200

    Add wptouch child

---
+++ wp-content/wptouch-data/themes/mobilestore-child/default/js/kaeufersiegel.js
@@ -0,0 +1,6 @@
+(function(i,s,o,g,r,a,m){a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
+})(window,document,'script','https://kaeufersiegel.de/bewertung/widgetCore/get-widget.js');
+
+(function(i,a,b,c,d){
+    i.ksId = a;i.ksDomain = b;i.ksWidgetDesign = c;i.ksWidgetWidth = d;
+})(window,'224e5e49814ca908e58c02e28a0462c1','ha-lehmann-at',1,80);
