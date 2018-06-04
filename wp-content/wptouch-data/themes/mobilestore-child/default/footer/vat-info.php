82f33cf7cc6ae48a4f3a94eafce498fe86fc7ad5
anito <axelnitzschner@mac.com>
2018-06-04 18:23:25 +0200

    css

--- wp-content/wptouch-data/themes/mobilestore-child/default/footer/vat-info.php
+++ wp-content/wptouch-data/themes/mobilestore-child/default/footer/vat-info.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 ?>
 <?php if ( get_option('woocommerce_gzd_small_enterprise') != 'yes' ) : ?>
 	<p class="footer-info vat-info"><?php echo ( get_option( 'woocommerce_tax_display_shop' ) == 'incl' ) ? __( 'All prices incl. VAT.', 'woocommerce-germanized' ) : __( 'All prices excl. VAT.', 'woocommerce-germanized' ) ?> zzgl. <span><a href="/versandarten/" style="color:#d30001;"> Versandkosten</a></span></p>
-	<p class="footer-info sale-info">Copyright &copy; <?php echo date('Y'); ?>&nbsp;<span>HA Lehmann</span><sup>&reg;</sup>
+	<p class="footer-info sale-info">Copyright &copy; <?php echo date('Y'); ?>&nbsp;<span>HA Lehmann</span><sup>&reg;</sup><br/>
 		<span class="">Design by </span><span class="powered-by-webpremiere"><a href="https://webpremiere.de/" target="_blank"><img class="logo" src="https://webpremiere.de/files/public-docs/logos/webpremiere-logo-5.svg" alt="WebPremiere"></a><sup>&reg;</sup></span>
 	</p>
 <?php else : ?>
