110c84250b794cd9ac6af97172ad5dd6a344fbb6
anito <axelnitzschner@mac.com>
2018-12-17 13:48:04 +0100

    Paginated galleries

--- wp-content/wptouch-data/themes/mobilestore-child/default/js/fancybox-helper.js
+++ wp-content/wptouch-data/themes/mobilestore-child/default/js/fancybox-helper.js
@@ -34,8 +34,6 @@
                 outer = '<a></a>';
                 inner.wrap($(outer));
             }
-//            console.log(outer)
-//            outer = $(outer);
             $(outer).attr( {
                 'href': src_big(this.src),
                 'data-fancybox' : 'gallery-' + galleryIndex,
