b92aee9eb7dd646a3e274a53b1c72ef352ee711a
anito <axelnitzschner@mac.com>
2019-01-10 19:40:01 +0100

    updates

--- wp-content/themes/vendipro-child/woocommerce/myaccount/my-account.php
+++ wp-content/themes/vendipro-child/woocommerce/myaccount/my-account.php
@@ -11,31 +11,26 @@
  * the readme will list any important changes.
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
- * @author  WooThemes
  * @package WooCommerce/Templates
- * @version 2.6.0
+ * @version 3.5.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+defined( 'ABSPATH' ) || exit;
 
-wc_print_notices();
-?>
-
-<div class="entry-post wrapper">
-	
-<?php
 /**
  * My Account navigation.
+ *
  * @since 2.6.0
  */
-do_action( 'woocommerce_account_navigation' ); ?>
-
+?>
+<div class="entry-post wrapper">
+	<?php do_action( 'woocommerce_account_navigation' ); ?>
+	
 	<div class="woocommerce-MyAccount-content">
 		<?php
 			/**
 			 * My Account content.
+		 *
 			 * @since 2.6.0
 			 */
 			do_action( 'woocommerce_account_content' );
