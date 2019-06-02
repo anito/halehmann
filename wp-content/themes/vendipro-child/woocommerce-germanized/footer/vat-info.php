6995df51c68b9e76b6c2ef5218dcb6cd7b66af6f
anito <axelnitzschner@mac.com>
2019-06-02 03:29:27 +0200

    Design by

--- wp-content/themes/vendipro-child/woocommerce-germanized/footer/vat-info.php
+++ wp-content/themes/vendipro-child/woocommerce-germanized/footer/vat-info.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 <?php if ( get_option('woocommerce_gzd_small_enterprise') != 'yes' ) : ?>
 	<p class="footer-info vat-info"><?php echo ( get_option( 'woocommerce_tax_display_shop' ) == 'incl' ) ? __( 'All prices incl. VAT.', 'woocommerce-germanized' ) : __( 'All prices excl. VAT.', 'woocommerce-germanized' ) ?> zzgl. <span><a href="/versandarten/" style="color:#d30001;"> Versandkosten</a></span></p>
 	<p class="footer-info sale-info">Copyright &copy; <?php echo date('Y'); ?>&nbsp;<span>Lehmann Trading GmbH</span><sup>&reg;</sup>
-		<span class="">Design by Axel Nitzschner<sup>&reg;</sup></span>
+		<span class="">Design by Axel Nitzschner</span>
 	</p>
 <?php else : ?>
 	<p class="footer-info vat-info"><?php echo wc_gzd_get_small_business_notice(); ?></p>
