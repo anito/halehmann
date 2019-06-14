5f04d36c28de0d1906085ee1d010a01f2895feb7
anito <axelnitzschner@mac.com>
2019-06-14 23:41:37 +0200

    Changed text Out of Stock

--- wp-content/themes/vendipro-child/functions.php
+++ wp-content/themes/vendipro-child/functions.php
@@ -737,7 +737,7 @@ function wcs_custom_get_availability( $availability, $_product ) {
    
    	// Change In Stock Text
     if ( $_product->is_in_stock() ) {
-        $availability['availability'] = __('In Stock', 'woocommerce');
+        $availability['availability'] = __('In stock', 'woocommerce');
     }
     // Change Out of Stock Text
     if ( ! $_product->is_in_stock() ) {
