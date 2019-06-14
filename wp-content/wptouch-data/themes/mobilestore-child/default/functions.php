d555de7b35c5ea4c4abd1b1947d6f754805dc4ea
anito <axelnitzschner@mac.com>
2019-06-14 23:26:32 +0200

    Changed text Out of Stock

--- wp-content/wptouch-data/themes/mobilestore-child/default/functions.php
+++ wp-content/wptouch-data/themes/mobilestore-child/default/functions.php
@@ -320,7 +320,7 @@ function wcs_custom_get_availability( $availability, $_product ) {
    
    	// Change In Stock Text
     if ( $_product->is_in_stock() ) {
-        $availability['availability'] = __('Available!', 'woocommerce');
+        $availability['availability'] = __('available', 'woocommerce');
     }
     // Change Out of Stock Text
     if ( ! $_product->is_in_stock() ) {
