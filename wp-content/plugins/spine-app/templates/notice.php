4681a5d9db5019ad649fc00208402be854da6a87
anito <axelnitzschner@mac.com>
2019-08-10 11:12:06 +0200

    Spine App Plugin

--- wp-content/plugins/spine-app/templates/notice.php
+++ wp-content/plugins/spine-app/templates/notice.php
@@ -8,8 +8,8 @@ foreach ($notices as $key => $notice) {
         <p>
             <div id="<?php echo $key; ?>" style="float:left;mmargin-top:3px;">Loading...</div>
             <div id="opt-button-wrapper" style="float:right">
-                <i id="opt-db-saving" class="wp-menu-image dashicons-before dashicons-cloud is-saving hide" style="text-align: right;width: 30px;display: inline-block;"></i>
-                <button id="opt-dump" class="button button-primary" style="min-width:150px;" data-type="dump" data-href="/api/mysql/mysql/dump">Jetzt sichern</button>
+                <i id="opt-db-saving" class="wp-menu-image dashicons-before dashicons-cloud is-saving hide" style="vertical-align: bottom; margin-right: 10px; display: inline-flex;"></i>
+                <button id="opt-dump" class="button button-primary" style="min-width: 150px;" data-type="dump" data-href="/api/mysql/mysql/dump">Jetzt sichern</button>
             </div>
             <div style="clear:both;"></div>
         </p>
