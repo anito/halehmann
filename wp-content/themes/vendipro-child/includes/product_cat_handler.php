b92b937b3393436bc647786adf450ffa41b3ed59
anito <axelnitzschner@mac.com>
2020-02-26 11:46:44 +0100

    sales checker changes

--- wp-content/themes/vendipro-child/includes/product_cat_handler.php
+++ wp-content/themes/vendipro-child/includes/product_cat_handler.php
@@ -1,15 +1,15 @@
 <?php
 function process_sales_cat($post_id, $term_id) {
 	global $woocommerce;
-	
+
 	if( !defined( 'SALES_CAT_ID' ) )
 		return;
-	
+
 	$post_id = intval($post_id);
 	$term_id = intval($term_id);
-	
+
 	$product = wc_get_product($post_id);
-	
+
 	switch ($term_id) {
 		case SALES_CAT_ID:
 			$add = $product->is_on_sale();
@@ -18,12 +18,12 @@ function process_sales_cat($post_id, $term_id) {
 			return 0;
 	}
 	set_product_cats($product, $term_id, $add);
-	
+
 }
 function set_product_cats($product, $term_id, $add) {
 	$term_ids = $product->get_category_ids();
 	$term_ids = array_unique(array_map('intval', $term_ids));
-	
+
 	if ( !$add ) {
 		# remove id
 		$term_ids = array_diff($term_ids, array($term_id) );
