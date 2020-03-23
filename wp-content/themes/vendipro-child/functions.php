f9566e4102f6af152f7729c06bd14e8f4992c74d
anito <axelnitzschner@mac.com>
2020-03-23 16:37:17 +0100

    removed unnecessary lines in function.php

--- wp-content/themes/vendipro-child/functions.php
+++ wp-content/themes/vendipro-child/functions.php
@@ -268,7 +268,6 @@ function vp_child_theme_styles() {
 	wp_enqueue_style( 'child-style-payments', get_stylesheet_directory_uri() . '/css/payments.css', wp_get_theme()->get('Version') );
 	wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'vendipro' ), wp_get_theme()->get('Version') );
 	// wp_dequeue_style('fontawesome');
-	// wp_dequeue_style('vendipro');
 	// wp_enqueue_style( 'fontawesome', get_stylesheet_directory_uri() . '/assets/font-awesome/css/all' . (IS_PRODUCTION ? '.min' : '') . '.css', wp_get_theme()->get('Version'));
 }
 
