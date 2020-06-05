bdc78a33fe8cbc17af4785dc86b8234f076fdba4
anito <axelnitzschner@mac.com>
2020-06-06 00:52:01 +0200

    css 7

--- wp-content/themes/vendipro-child/js/age-gate/age-gate.js
+++ wp-content/themes/vendipro-child/js/age-gate/age-gate.js
@@ -2,12 +2,12 @@
     'use strict';
 
     $.add_listener = function () {
-        
+
         $('.age-gate-form .goback').on( 'click', function( e ) {
             e.preventDefault();
             history.go(-1);
         })
-        
+
     }
 })(jQuery)
 jQuery.add_listener();
