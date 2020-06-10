6ba69c4cc64d5e622014df80cb5c0ecaa0bc4d3d
anito <axelnitzschner@mac.com>
2020-06-10 18:14:25 +0200

    wip 14

--- wp-content/themes/flatsome-child/template-parts/footer/sub-footer.php
+++ wp-content/themes/flatsome-child/template-parts/footer/sub-footer.php
@@ -1,11 +1,11 @@
 <!-- FOOTER 3 -->
 <div class="footer-widgets footer footer-3">
 		<div class="<?php echo flatsome_footer_row_style('footer-2'); ?> mb-0">
-		<?php dynamic_sidebar( 'vp_sub_footer' ); ?>
+		<?php dynamic_sidebar( 'wbp_sub_footer_1' ); ?>
 		</div>
 </div>
 <div class="footer-widgets footer footer-3">
 		<div class="<?php echo flatsome_footer_row_style('footer-2'); ?> mb-0">
-		<?php dynamic_sidebar( 'vp_sub_footer_2' ); ?>
+		<?php dynamic_sidebar( 'wbp_sub_footer_2' ); ?>
 		</div>
 </div>
